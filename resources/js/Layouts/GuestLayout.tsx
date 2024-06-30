df2ff169498e3db81446bbf2b5b51a27353552b6
Faizul Amir <faizul.ramir@gmail.com>
2024-06-30 18:25:15 +0800

    center

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,15 +4,17 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <div className="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div className="w-full sm:max-w-md mx-auto">
+                <div className="flex justify-center">
+                    <Link href="/">
+                        <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                    </Link>
+                </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
+                <div className="w-full mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                    {children}
+                </div>
             </div>
         </div>
     );
